c724e0bb47149a0467d9d9c227e30c7b9c50788c
nulluser <nulluser@gmail.com>
2011-11-27 13:30:13 -0500

    Added monitor

--- monitor.h
+++ monitor.h
@@ -31,11 +31,25 @@
 #include <unistd.h>
 
 
+
+
+
+
 #include "mnode.h"
 #include "datatap.h"
 #include "network.h"
 
 #define MODULE_NAME "[MNode] "
+
+
+int main_running;
+
+
+
+#define STATE_WAIT      0
+#define STATE_LENGTH    1
+#define STATE_DATA      2
+
 
 
 
@@ -118,32 +132,130 @@ void mesh_packet(unsigned char *data, unsigned int length)
 
 
 
- 
- int mnode_start(void)
- {
- 
- 
+
+/* Menu command - exit */
+void command_exit( void )
+{
+    main_running = 0;
+}
+/* End of command_exit */
+
+/* Menu command - send */
+void command_ident( void )
+{
+    network_ident();
+}
+/* End of command_send */
+
+
+
+/* Menu command - help */
+void command_string( char *s )
+{
+    network_string((unsigned char *)s);
+
+}
+/* End of command_help */
+
+
+/* Menu command - help */
+void command_nodes( void )
+{
+    network_list_nodes();
+
+}
+/* End of command_help */
+
+
+
+void command_data( void )
+{
+    printf("Not implemented\n");
+}
+
+
+void command_nodedata( void )
+{
+    printf("Sending Request\n");
+    network_datatap_poll();
+}
+
+
+
+
+/* Menu command - help */
+void command_help( void )
+{
+    printf("Commands\n");
+    printf("exit        Quit program\n");
+    printf("ident       Send ident\n");
+    printf("send        Send string\n");
+    printf("nodes       List nodes\n");
+    printf("data        List data for this node\n");
+    printf("nodedata    List all nodedata\n");
+    
+    printf("help        Display this message\n");
+}
+/* End of command_help */
+
+
+
+
+/* Main */
+int main ( void )
+{
+    char  *s;
+    size_t len = 0;
+    int    b;
+
+    printf(MODULE_NAME "Startup\n");
+
+    main_running = 1;
+
     network_start(mesh_packet);
 
- 
- return 0;
- }
- 
- 
+       
+    while(main_running)
+    {
+        printf(">");
 
- 
- int mnode_stop(void)
- {
- 
- 
+        b = getline(&s, &len, stdin);
+                
+        if (0 < b) s[b-1] = 0; // Erase netline
+        
+        
+        if (!strcmp(s, "exit")) command_exit();
+        if (!strcmp(s, "ident")) command_ident();
+        if (!strcmp(s, "help")) command_help();
+        if (!strcmp(s, "nodes")) command_nodes();
+        if (!strcmp(s, "data")) command_data();
+        if (!strcmp(s, "nodedata")) command_nodedata();        
+        if (!strcmp(s, "send")) 
+        {
+        printf("Message: ");
+         b = getline(&s, &len, stdin);
+        command_string(s);
+        }
+
+    }
+    
+    network_stop();
+    
     return 0;
  }
+ /* End of main */
  
  
  
  
  
  
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
